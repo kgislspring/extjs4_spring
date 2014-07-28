5f22faf8d327a2c672de0e8923c8af4522ac39cb
kgislspring <Ravishankar@kggroup.com>
2014-07-28 16:43:51 +0530

    Removed existing files

--- EXTJS4FileUpload/src/com/extjs/java/dao/FileUploadDAO.java
+++
@@ -1,9 +0,0 @@
-package com.extjs.java.dao;
-
-import java.io.InputStream;
-
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FileUploadDAO {
-public boolean saveFiletoDB(String filename,int filesize,InputStream filecontent);
-}
